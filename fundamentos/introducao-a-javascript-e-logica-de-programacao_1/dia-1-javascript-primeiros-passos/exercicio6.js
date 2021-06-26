6f0f19ec50f662c597b5f9effc138a94787963d6
Dayane <dayanebmartins@gmail.com>
2021-06-26 18:53:24 -0300

    desenvolvendo exe 6

---
+++ fundamentos/introducao-a-javascript-e-logica-de-programacao_1/dia-1-javascript-primeiros-passos/exercicio6.js
@@ -0,0 +1,34 @@
+//exercise.js
+/* Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+*/
+
+
+/* Qual é a posição da RAINHA?
+Qual é a posição da peça?
+Quando a passagem será invalida?
+Quais caminhos a Rainha pode ir?
+/*
+/- Qual a posicao da Rainha?
+*/
+let colunaAtualRainha=4;
+let linhaAtualRainha=4;
+
+/* Qual é a posição da peça?*/
+let colunaAtualPeca=6;
+let linhaAtualPeca=6;
+
+/*Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.*/
+let ataqueBemSucedido=false;
+
+
+
+/*Quais caminhos a Rainha pode ir? HORIZONTAL, VERTICAL, DIAGONAL,*/
+for (let indexlinha = 1; index < linha.length; indexlinha++) {
+  
+    }
+
+    
+}
+
+
