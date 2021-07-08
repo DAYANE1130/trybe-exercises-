cb76916f3e7374935075113379266436a373fa24
Dayane <dayanebmartins@gmail.com>
2021-07-08 15:01:10 -0300

    finaliza exer 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exer5-3.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exer5-3.js
@@ -1,45 +1,59 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
   };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+
+const diasmesli = document.querySelector('#days');
+for (let index = 0; index < dezDaysList.length; index += 1) {
+  const cadadia = dezDaysList[index];
+
+  let listaDias = document.createElement('li');
+  listaDias.innerHTML = cadadia;
+
+  listaDias.classList.add('day');
+  diasmesli.appendChild(listaDias);
+
+  if (cadadia === 24 || cadadia === 31) {
+    listaDias.classList.add('holiday')
+  }
+  if (cadadia === 25) {
+    listaDias.classList.add('friday')
+    listaDias.classList.add('holiday')
+  }
+  if (cadadia === 18 || cadadia === 11 || cadadia === 18) {
+    listaDias.classList.add('friday')
+  }
+}
+
+function criandoBotaoHoliday(botaonome){
+  let botao= document.getElementsByClassName("buttons-container")[0]
   
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
+  let novoBotao=document.createElement('button')
 
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+  let novoBotaoId= 'btn-holiday';
 
-  const diasmesli=document.querySelector('#days');
-   for (let index = 0; index < dezDaysList.length; index+=1) {
-     const cadadia= dezDaysList[index];
+  novoBotao.id=novoBotaoId;
+  novoBotao.innerHTML= botaonome;
+  botao.appendChild(novoBotao)
+}
 
-     let listaDias=document.createElement('li');
-     listaDias.innerHTML=cadadia;
-     
-     diasmesli.appendChild(listaDias)
-     listaDias.classList.add('day');
+criandoBotaoHoliday("Feriados");
 
-      
-     if (cadadia===24 || cadadia===31){
-    
-       listaDias.classList.add('holiday')
-     }
-     if (cadadia===25){
-       listaDias.classList.add('friday')
-       listaDias.classList.add('holiday')
-     }
-      if(cadadia===18 || cadadia===11 || cadadia===18){
-        listaDias.classList.add('friday')
-      }
 
-     }
-     
-   
+
+
+
