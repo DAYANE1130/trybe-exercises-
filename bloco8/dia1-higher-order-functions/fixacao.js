c14567d681d86d4e2f715e9fe2b00b7d98fc2a56
Dayane <dayanebmartins@gmail.com>
2021-08-02 12:30:28 -0300

    adiciona exemplos

--- bloco8/dia1-higher-order-functions/fixacao.js
+++ bloco8/dia1-higher-order-functions/fixacao.js
@@ -1,6 +1,32 @@
-// Ao chamar a função doingThings:
-const wakeUp = () => 'Acordando!!';
-doingThings(wakeUp);
+function sum (number1, number2) {
+  return number1 + number2;
+}
 
-// Ela deve retornar o valor do respectivo parâmetro, neste caso:
-// Acordando!!
+const sumVariable = sum;
+
+console.log(sumVariable);
+//  [Function: sum]
+
+const sum = (number1, number2) => {
+  return number1 + number2;
+};
+ sum(1,2);
+console.log(sum (1,2));
+
+const sayHello = () => {
+  return ('hello trybers');
+}
+
+const printGreeting = (callback) => {
+    console.log(callback());
+}
+
+printGreeting(sayHello);
+
+const sumFixAmount = (amount) => {
+  return (number) => amount + number;
+
+}
+
+const initialSum = sumFixAmount(15)
+console.log(initialSum(6));
