b196d4143a036685962209819916e254d4662a47
Dayane <dayanebmartins@gmail.com>
2021-08-03 15:09:07 -0300

    adiciona ex 1 do 8.1

---
+++ bloco8/dia1-higher-order-functions/exer1-8.1.js
@@ -0,0 +1,20 @@
+
+const pessoaContratada=(nomeCompleto)=>{
+
+  const email= nomeCompleto.toLowerCase().split(' ').join('_');
+  return  {nomeCompleto :nomeCompleto , email:`${email}@betrybe.com`};
+      
+  }
+const newEmployees = (pessoaContratada) => {
+
+  const employees = {
+    id1: pessoaContratada('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: pessoaContratada( 'Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: pessoaContratada ('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  
+  }
+  return employees;
+};
+
+
+console.log(newEmployees(pessoaContratada));
